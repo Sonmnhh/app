800fb16cafdb552307effd194be00576c4e78359
Sonmnhh <minhson2118@gmail.com>
2024-06-13 21:25:07 +0700

    Update demo_streamlit.py

--- demo_streamlit.py
+++ demo_streamlit.py
@@ -12,30 +12,52 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import pandas as pd
 import numpy as np
 import pickle
-
+import requests
+import tempfile
 
 
 # In[8]:
 
-# Define the exact paths for the tokenizer and model
-tokenizer_path = r"C:\Users\ASUS\Desktop\k2 2023-2024\Các hệ thống thông tin nâng cao\Final\tokenizer.pkl"
-model_path = r"C:\Users\ASUS\Desktop\k2 2023-2024\Các hệ thống thông tin nâng cao\Final\finbert_model"
+# Define the URLs for the tokenizer and model
+tokenizer_url = 'https://drive.google.com/file/d/1nM-nXY308A0CtEwAUCq_Mhv5kde6yRO0/view?usp=drive_link'
+model_url = 'https://drive.google.com/file/d/1QiZrC53r_Dqn5160rGvnHGzpzTG0hbqm/view?usp=drive_link'
+
+# Download the tokenizer
+@st.cache_data
+def download_tokenizer(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.pkl') as tmp_file:
+        tmp_file.write(response.content)
+        return tmp_file.name
+
+tokenizer_path = download_tokenizer(tokenizer_url)
 
 # Load the tokenizer
 try:
     with open(tokenizer_path, "rb") as f:
         tokenizer = pickle.load(f)
-except FileNotFoundError:
-    st.error(f"Error loading the tokenizer: No such file or directory: '{tokenizer_path}'")
+except Exception as e:
+    st.error(f"Error loading the tokenizer: {e}")
     st.stop()
+
+# Download the model
+@st.cache_data
+def download_model(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(response.content)
+        return tmp_file.name
+
+model_path = download_model(model_url)
 
 # Load the model
 try:
     model = AutoModelForSequenceClassification.from_pretrained(model_path)
-except FileNotFoundError:
-    st.error(f"Error loading the model: No such file or directory: '{model_path}'")
+except Exception as e:
+    st.error(f"Error loading the model: {e}")
     st.stop()
-
 # In[9]:
 
 
@@ -59,7 +81,7 @@ def predict_sentiment(text):
 # Sidebar navigation
 st.sidebar.title("For Financial")
 page = st.sidebar.radio("Go to", ["Home", "News", "Contact Me"])
-        
+
 # Add custom CSS for styling
 st.markdown(
     """
@@ -114,7 +136,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-        
+
 st.title("Financial Sentiment Analysis WebApp.")  
 st.write('Welcome to my sentiment analysis app!')
 
